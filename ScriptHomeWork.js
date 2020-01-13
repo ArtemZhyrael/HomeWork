c9964646ba512b43fd474a888be8ebb56839e7d3
Artem <zhyravelart@gmail.com>
2020-01-13 16:21:15 +0300

    Lesson2a

--- ScriptHomeWork.js
+++ ScriptHomeWork.js
@@ -14,7 +14,7 @@ for (let i =0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце"),
         b = prompt("Во сколько обойдется?");
 
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
         && a != '' && b != '' && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
@@ -29,11 +29,11 @@ appData.moneyPerDay = appData.salary / 30;
 alert("Ежедннневный бюджет: " + appData.moneyPerDay);
 
 if(appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка")
+    console.log("Минимальный уровень достатка");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка")
+    console.log("Средний уровень достатка");
 } else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень дотстатка")
+    console.log("Высокий уровень дотстатка");
 } else {
-    console.log("Произошла ошибка")
+    console.log("Произошла ошибка");
 }
