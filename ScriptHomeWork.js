23f3bf41236d28ee9b14caa97292fa8a2469e209
Artem <zhyravelart@gmail.com>
2020-01-16 15:28:31 +0300

    lesson5

--- ScriptHomeWork.js
+++ ScriptHomeWork.js
@@ -65,5 +65,25 @@ let appData = {
                 appData.monthIncome = save/100/12*percent;
                 alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
         }
+    },
+    chooseIncome : function() {
+        let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
+
+        if (typeof(items) != "string" || items == "" || typeof(items) == null) {
+            console.log("Вы ввели некорректные данные или не ввели их вовсе");
+        } else {
+            appData.income = items.split(", ");
+            appData.income.push(prompt("Может что-то еще?"));
+            appData.income.sort();
+        }
+
+        appData.income.forEach (function (itmass, i) {
+            alert("Способы доп. заработка: " + (i+1) + " - " + itmass);
+        });
     }
 };
+
+for (let i in appData) {
+    console.log("Наша программа включает в себя данные: " + i + " - " + appData[i]);
+}
+
